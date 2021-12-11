931dae2a6d7c282a27ab247dbfe7bf8c9a73fa8a
tykimdream@korea.ac.kr <tykimdream@korea.ac.kr>
2021-12-11 13:18:39 +0900

    final exam

---
+++ prac.js
@@ -0,0 +1,34 @@
+// 구버젼 함수만들기
+const functionLiteral = function() {
+    return 'a';
+
+}
+
+// 신버젼 함수 만들기 (화살표 함수)
+const functionLiteral_new = () => {
+    return 'a';
+}
+
+const array = ['가', '나', '다'];
+
+// for(var i in array){
+//     console.log(array[i]);
+// }
+
+// for(let item of array){
+//     console.log(item);
+// }
+
+for(var i = 0; i<array.length; i++){
+    var index = i;
+    var item = array[i];
+    console.log(item + ' : ' + index);
+}
+
+array.forEach((item, index) => {
+    console.log(`${item} : ${index}`);
+})
+
+array.forEach((item, index) => {
+    console.log(`${item} : ${index} `);
+})
