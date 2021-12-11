940c4484299709567b018d02db1f531e41846c90
tykimdream@korea.ac.kr <tykimdream@korea.ac.kr>
2021-12-11 21:05:49 +0900

    farm stat

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 12.7
 javascript project established
 
-
+12.11
+game building
 
--- game/action.js
+++ game/action.js
@@ -3,14 +3,17 @@ const { parse } = require("path/posix");
 function count(type)  {
     const resultElement = document.getElementById('hp');
     const dayElemnt = document.getElementById('day');
-    
+    const actElement = document.getElementById('act_count');
+
     let number = resultElement.innerText;
     let night = dayElemnt.innerText;
+    let count = actElement.innerText;
     
     // 더하기/빼기
     if(type === 'water') {
         if(parseInt(number) - 8 > 0){
             number = parseInt(number) - 8;
+            count = parseInt(count)+1;
         }
         else{
             alert('에너지가 부족합니다.')
@@ -18,13 +21,14 @@ function count(type)  {
     }
     else if(type === 'sleep')  {
         alert('정말 주무시겠습니까?.')
-      number = 100;
+      number = parseInt(number) + 70;
       night = parseInt(night)+1;
     }
     
     // 결과 출력
     resultElement.innerText = number;
     dayElemnt.innerText = night;
+    actElement.innerText = count;
   }
 
   
